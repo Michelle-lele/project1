bff02e5ac188968e0276b7d338900b9ef75653b9
Michelle-lele <fia_m@abv.bg>
2019-04-14 19:22:43 +0300

    User registration - basic

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,7 @@
 import os
+import sys
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +24,47 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+
+
+	if request.method == "POST":
+		errorMessages= []
+		#ensure required fields are submitted
+		if not request.form.get("username"):
+			errorMessages.append("Username is required!")
+		
+		if not request.form.get("password"):
+			errorMessages.append("Password is required!")
+
+		if not request.form.get("confirm_password"):
+			errorMessages.append("Confirm Password is required!")
+
+		if errorMessages != []:
+			return render_template("signup.html", errorMessages=errorMessages)
+
+		#ensure passwords match
+		if request.form.get("password") != request.form.get("confirm_password"):
+			errorMessages.append("Passwords don't match!")
+			return render_template("signup.html", errorMessages=errorMessages)
+
+		#ToDo password validation
+
+		#ensure username doesn't exist
+		NewUserName = request.form.get("username")
+		usernames= db.execute("SELECT username From users").fetchall()
+		#print(usernames, file=sys.stderr)
+		for i in range(len(usernames)):
+			#print(NewUserName, file=sys.stderr)
+			#print(usernames[i][0], file=sys.stderr)
+			if NewUserName == usernames[i][0]:
+				errorMessages.append("Username already exists!")
+				return render_template("signup.html", errorMessages=errorMessages)
+		
+		#create new user in db
+		NewUser = db.execute("INSERT INTO users (username, password, name) VALUES (:username,:password, :name)", 
+			{"username": request.form.get("username"), "password": request.form.get("password"), "name": request.form.get("name")})
+		db.commit()
+		#return render_template("index.html")
+		
+	return render_template("signup.html")
+
+ 
