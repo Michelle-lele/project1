c02203e6e2567df05be29e7d8eda95843a4091b6
Michelle-lele <fia_m@abv.bg>
2019-06-03 20:15:31 +0300

    Well unknown changes :D

--- application.py
+++ application.py
@@ -240,19 +240,22 @@ def book():
 	if request.method == "GET":
 		if not request.args.get("isbn"):
 			return redirect("search")
+	isbn= request.args.get("isbn")
 	book_details = db.execute("SELECT title, author, isbn, year from books WHERE isbn = :isbn", 
 			{"isbn": request.args.get("isbn")}).fetchone()
+	print(isbn, file=sys.stderr)
 	#print(book_details, file=sys.stderr)
 	if request.method == "POST":
 		errorMessages = []
 		if not request.form.get("rating"):
 			errorMessages.append("Rating is required!")
-		print(request.form.get("rating"), file=sys.stderr)
+		#print(request.form.get("rating"), file=sys.stderr)
 		if not request.form.get("review_text"):
 			errorMessages.append("Review text is required!")
 		if errorMessages != []:
 			return render_template("book.html", username = username, book_details = book_details, errorMessages=errorMessages)
-	return render_template("book.html", username = username, book_details = book_details)
+		return redirect(url_for('book',**request.args))
+	return render_template("book.html", username = username, book_details = book_details, isbn = isbn)
 
 
 
